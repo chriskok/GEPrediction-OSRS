17c1385771d362ed956e38ee7945da90a39e975d
Christopher Kok <ckok@purdue.edu>
2019-12-09 19:28:35 -0600

    Updated tests for showcase

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-my_list = list(range(554,567,1))
+import requests
+import json
 
-print(my_list)
+itemID = 560
+r = requests.get('http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json', params={'item': itemID})
+json_data = json.loads(r.text)
+json_formatted_str = json.dumps(json_data, indent=2)
+
+print(json_formatted_str)
