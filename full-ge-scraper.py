4a5ac73f7e2cce332b3be2337c007d017348e5b7
Christopher Kok <ckok@purdue.edu>
2019-12-25 06:43:40 +0800

    Updated full scraper to work with task scheduler

--- full-ge-scraper.py
+++ full-ge-scraper.py
@@ -2,94 +2,90 @@ import requests
 import json
 import csv
 import time
+import os.path
 
 rsbuddyAPI = "https://rsbuddy.com/exchange/summary.json"
 
 # One dict each for 'buy_average', 'buy_quantity', 'sell_average', 'sell_quantity', 'overall_average', 'overall_quantity'
-buy_average = {}
-buy_quantity = {}
-sell_average = {}
-sell_quantity = {}
-overall_average = {}
-overall_quantity = {}
+buy_average = []
+buy_quantity = []
+sell_average = []
+sell_quantity = []
+overall_average = []
+overall_quantity = []
 
 labels = ['timestamp']
-chosen_ids = []
-MINUTES_TO_WAIT = 5
+
+def writeToCSV(filename, data, timestamp):
+    with open('data/{}.csv'.format(filename), mode='w', newline='') as GE_data:
+        GE_writer = csv.writer(GE_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        GE_writer.writerow(labels)  # write field names
+
+        new_array = [timestamp]
+        new_array.extend(data)
+        GE_writer.writerow(new_array)
+
+
+def appendToCSV(filename, data, timestamp):
+    with open('data/{}.csv'.format(filename), mode='a', newline='') as GE_data:
+        GE_writer = csv.writer(GE_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        new_array = [timestamp]
+        new_array.extend(data)
+        GE_writer.writerow(new_array)
 
 def initialize_data():
     # Get the seconds since epoch
     current_timestamp = int(time.time())
     print("{} - initializing data".format(current_timestamp))
-    buy_average[current_timestamp] = []
-    buy_quantity[current_timestamp] = []
-    sell_average[current_timestamp] = []
-    sell_quantity[current_timestamp] = []
-    overall_average[current_timestamp] = []
-    overall_quantity[current_timestamp] = []
 
     r = requests.get(rsbuddyAPI)
     json_data = json.loads(r.text)
     for item in json_data:
-        if (json_data[item]["overall_quantity"] > 5 and json_data[item]["members"] == False):
-            labels.append(json_data[item]["name"].replace(" ", "_"))
-            chosen_ids.append(json_data[item]["id"])
-            buy_average[current_timestamp].append(json_data[item]["buy_average"])
-            buy_quantity[current_timestamp].append(json_data[item]["buy_quantity"])
-            sell_average[current_timestamp].append(json_data[item]["sell_average"])
-            sell_quantity[current_timestamp].append(json_data[item]["sell_quantity"])
-            overall_average[current_timestamp].append(json_data[item]["overall_average"])
-            overall_quantity[current_timestamp].append(json_data[item]["overall_quantity"])
+        labels.append(json_data[item]["name"].replace(" ", "_"))
+        buy_average.append(json_data[item]["buy_average"])
+        buy_quantity.append(json_data[item]["buy_quantity"])
+        sell_average.append(json_data[item]["sell_average"])
+        sell_quantity.append(json_data[item]["sell_quantity"])
+        overall_average.append(json_data[item]["overall_average"])
+        overall_quantity.append(json_data[item]["overall_quantity"])
+    
+    writeToCSV("buy_average", buy_average, current_timestamp)
+    writeToCSV("buy_quantity", buy_quantity, current_timestamp)
+    writeToCSV("sell_average", sell_average, current_timestamp)
+    writeToCSV("sell_quantity", sell_quantity, current_timestamp)
+    writeToCSV("overall_average", overall_average, current_timestamp)
+    writeToCSV("overall_quantity", overall_quantity, current_timestamp)
 
 def append_data():
     # Get the seconds since epoch
     current_timestamp = int(time.time())
     print("{} - appending data".format(current_timestamp))
 
-    buy_average[current_timestamp] = []
-    buy_quantity[current_timestamp] = []
-    sell_average[current_timestamp] = []
-    sell_quantity[current_timestamp] = []
-    overall_average[current_timestamp] = []
-    overall_quantity[current_timestamp] = []
-
     r = requests.get(rsbuddyAPI)
     json_data = json.loads(r.text)
     for item in json_data:
-        if (json_data[item]["id"] in chosen_ids):
-            buy_average[current_timestamp].append(json_data[item]["buy_average"])
-            buy_quantity[current_timestamp].append(json_data[item]["buy_quantity"])
-            sell_average[current_timestamp].append(json_data[item]["sell_average"])
-            sell_quantity[current_timestamp].append(json_data[item]["sell_quantity"])
-            overall_average[current_timestamp].append(json_data[item]["overall_average"])
-            overall_quantity[current_timestamp].append(json_data[item]["overall_quantity"])
+        buy_average.append(json_data[item]["buy_average"])
+        buy_quantity.append(json_data[item]["buy_quantity"])
+        sell_average.append(json_data[item]["sell_average"])
+        sell_quantity.append(json_data[item]["sell_quantity"])
+        overall_average.append(json_data[item]["overall_average"])
+        overall_quantity.append(json_data[item]["overall_quantity"])
 
-def writeToCSV(filename, data):
-    with open('data/{}.csv'.format(filename), mode='w', newline='') as GE_data:
-        GE_writer = csv.writer(GE_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        GE_writer.writerow(labels)  # write field names
+    appendToCSV("buy_average", buy_average, current_timestamp)
+    appendToCSV("buy_quantity", buy_quantity, current_timestamp)
+    appendToCSV("sell_average", sell_average, current_timestamp)
+    appendToCSV("sell_quantity", sell_quantity, current_timestamp)
+    appendToCSV("overall_average", overall_average, current_timestamp)
+    appendToCSV("overall_quantity", overall_quantity, current_timestamp)
 
-        for time in data:
-            new_array = [time]
-            new_array.extend(data[time])
-            # print(len(new_array))
-            GE_writer.writerow(new_array)
 
 def main():
-    initialize_data()
-    time.sleep(MINUTES_TO_WAIT * 60)
 
-    while True:
+    if os.path.isfile('data/buy_average.csv'):
         append_data()
-
-        writeToCSV("buy_average", buy_average)
-        writeToCSV("buy_quantity", buy_quantity)
-        writeToCSV("sell_average", sell_average)
-        writeToCSV("sell_quantity", sell_quantity)
-        writeToCSV("overall_average", overall_average)
-        writeToCSV("overall_quantity", overall_quantity)
-
-        time.sleep(MINUTES_TO_WAIT * 60)
+    else:
+        initialize_data()
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,25 +1,52 @@
 import requests
 import json
 import time
+import csv
+import os.path
+import datetime
 
-rsbuddyAPI = "https://rsbuddy.com/exchange/summary.json"
+# rsbuddyAPI = "https://rsbuddy.com/exchange/summary.json"
 
-fullDict = {}
-labels = ['timestamp']
-allitems = []
+# fullDict = {}
+# labels = ['timestamp']
+# allitems = []
 
-def initialize_fullDict():
-    # Get the seconds since epoch
-    current_timestamp = int(time.time())
-    fullDict[current_timestamp] = []
+# def initialize_fullDict():
+#     # Get the seconds since epoch
+#     current_timestamp = int(time.time())
+#     fullDict[current_timestamp] = []
 
-    r = requests.get(rsbuddyAPI)
-    json_data = json.loads(r.text)
-    for item in json_data:
-        if (json_data[item]["overall_quantity"] > 5 and json_data[item]["members"] == False):
-            print(json_data[item])
-            allitems.append(item)
+#     r = requests.get(rsbuddyAPI)
+#     json_data = json.loads(r.text)
+#     for item in json_data:
+#         if (json_data[item]["overall_quantity"] > 5 and json_data[item]["members"] == False):
+#             print(json_data[item])
+#             allitems.append(item)
 
-    print(len(allitems))
+#     print(len(allitems))
 
-initialize_fullDict()
+# initialize_fullDict()
+
+def writeToCSV(filename):
+    with open('data/{}.csv'.format(filename), mode='w', newline='') as GE_data:
+        GE_writer = csv.writer(GE_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        GE_writer.writerow(['timestamp, random1, random2'])  # write field names
+
+        current_timestamp = int(time.time())
+        new_array = [current_timestamp, time.strftime('%Y-%m-%dT%H:%M:%S %Z',time.localtime(time.time())), 25]
+        GE_writer.writerow(new_array)
+
+def appendToCSV(filename):
+    with open('data/{}.csv'.format(filename), mode='a', newline='') as GE_data:
+        GE_writer = csv.writer(GE_data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        # GE_writer.writerow(['timestamp, random1, random2'])  # write field names
+        
+        current_timestamp = int(time.time())
+        new_array = [current_timestamp, time.strftime('%Y-%m-%dT%H:%M:%S %Z',time.localtime(time.time())), 25]
+        GE_writer.writerow(new_array)
+
+filename = 'test'
+if os.path.isfile('data/{}.csv'.format(filename)):
+    appendToCSV(filename)
+else:
+    writeToCSV(filename)
