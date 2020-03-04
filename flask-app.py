ef6e0ed0eb606f5e34378dc024c5248b8b8e7a0a
chriskok <ckok@purdue.edu>
2020-03-04 10:33:08 -0600

    Added D3 options and transitions

--- flask-app.py
+++ flask-app.py
@@ -6,49 +6,49 @@ app = Flask(__name__, template_folder='./templates', static_folder='./static')
 
 @app.route('/')
 def index():
-	item_predicted = 'Adamant_platebody'
-	df = pd.read_csv('data/predictions/{}.csv'.format(item_predicted))
-	print(df.head())
+	# items_predicted = ['Old_school_bond']
+	items_predicted = ['Old_school_bond', 'Rune_platebody', 'Adamant_platebody']
+	data = {}
+	names = {}
+	count = 0 
+	for item_predicted in items_predicted:
+		df = pd.read_csv('data/predictions/{}.csv'.format(item_predicted))
+		# print(df.tail(10))
 
-	buy_avg = pd.read_csv('data/rsbuddy/buy_average.csv')[['timestamp', item_predicted]]
-	buy_avg = buy_avg.set_index('timestamp')
-	buy_avg = buy_avg.drop_duplicates()
-	buy_avg = buy_avg.reset_index()
-	buy_avg = buy_avg.rename(columns={'timestamp': 'ts', item_predicted: 'real'})
-	print(buy_avg.head())
+		buy_avg = pd.read_csv('data/rsbuddy/buy_average.csv')[['timestamp', item_predicted]]
+		buy_avg = buy_avg.set_index('timestamp')
+		buy_avg = buy_avg.drop_duplicates()
+		buy_avg = buy_avg.reset_index()
+		buy_avg = buy_avg.rename(columns={'timestamp': 'ts', item_predicted: 'real'})
+		buy_avg = buy_avg.replace(to_replace=0, method='ffill')
+		# print(buy_avg.tail(10))
 
-	merged_df = pd.merge_asof(df, buy_avg, left_on='timestamp', right_on='ts', direction='backward')
-	chart_data = merged_df.to_dict(orient='records')
-	chart_data = json.dumps(chart_data, indent=2)
-	data = {'chart_data': chart_data}
-	print(data)
+		merged_df = pd.merge_asof(df, buy_avg, left_on='timestamp', right_on='ts', direction='backward')
+		chart_data = merged_df.to_dict(orient='records')
+		chart_data = json.dumps(chart_data, indent=2)
+		data['chart{}'.format(count)] = chart_data
+		names[count] = item_predicted
+		count += 1
+		# print(data)
 
-	return render_template("index.html", data=data)
+	return render_template("index.html", data=data, names=names)
 
 # A route to return all of the available entries in our catalog.
-@app.route('/api/item/', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def api_all():
-	with open('data/predictions/Old_school_bond.csv', mode='r') as infile:
-		reader = csv.reader(infile)
-		next(reader)
-		mydict = {rows[0]:rows[1] for rows in reader}
-		
-	# # Check if an ID was provided as part of the URL.
-	# # If ID is provided, assign it to a variable.
-	# # If no ID is provided, display an error in the browser.
-	# if 'id' in request.args:
-	#     id = int(request.args['id'])
-	# else:
-	#     return "Error: No id field provided. Please specify an id."
+	if 'name' in request.args:
+	    name = str(request.args['name'])
+	else:
+	    return "Error: No name field provided. Please specify an name."
 
-	# # Create an empty list for our results
-	# results = []
-
-	# # Loop through the data and match results that fit the requested ID.
-	# # IDs are unique, but other fields might return many results
-	# for book in books:
-	#     if book['id'] == id:
-	#         results.append(book)
+	try:
+		with open('data/predictions/{}.csv'.format(name), mode='r') as infile:
+			
+			reader = csv.reader(infile)
+			next(reader)
+			mydict = {rows[0]:rows[1] for rows in reader}
+	except EnvironmentError:
+		return "File not found. Please specify different name"	
 
 	return jsonify(mydict)
 
